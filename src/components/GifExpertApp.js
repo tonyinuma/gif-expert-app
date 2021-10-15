ddd8470477436986dcb8433b42eac734b989ee4f
Tony ID <tony.inuma@gmail.com>
2021-10-15 16:47:29 -0500

    Feat: Button to add categories

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const GifExpertApp = () => {
+    // const categories = ["One Punch", "Dragon Ball", "Samurai X"];
+    const [categories, setCategories] = useState(["One Punch", "Dragon Ball", "Samurai X"]);
+
+    const handleAdd = () => {
+        setCategories([...categories, 'Doraemon'])
+    }
+
     return (
         <>
-           <h2>GifExpertApp</h2> 
-           <hr/>
+            <h2>GifExpertApp</h2>
+            <hr />
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {categories.map((category) => {
+                    return <li key={category}>{category}</li>;
+                })}
+            </ol>
         </>
-    )
-}
+    );
+};
