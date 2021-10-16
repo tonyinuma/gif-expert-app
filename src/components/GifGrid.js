a46141e356c1aa129dc74d22412d710fbbbf53a2
Tony ID <tony.inuma@gmail.com>
2021-10-15 22:25:16 -0500

    Add: Format

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,16 +1,15 @@
-import React  from "react";
+import React from "react";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
-
-    const {data:images, loading} = useFetchGifs(category);
+    const { data: images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
 
-            { loading && <p>Loading.. </p>}
+            {loading && <p>Loading.. </p>}
 
             <div className="card-grid">
                 {images.map((image) => {
