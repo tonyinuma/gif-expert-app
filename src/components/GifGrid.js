8cc099966c8f7a641073c3ef9dd674fd2aefa69e
Tony ID <tony.inuma@gmail.com>
2021-10-15 22:41:13 -0500

    Add: Animate css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -7,9 +7,13 @@ export const GifGrid = ({ category }) => {
 
     return (
         <>
-            <h3>{category}</h3>
+            <h3 className="animate__animated animate__fadeInRight">{category}</h3>
 
-            {loading && <p>Loading.. </p>}
+            {loading && (
+                <p className="animate__animated animate__rotateIn">
+                    Loading..{" "}
+                </p>
+            )}
 
             <div className="card-grid">
                 {images.map((image) => {
--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const GifGridItem = (image) => {
     return (
-        <div className="card">
+        <div className="card animate__animated animate__bounceIn">
             <img src={image.url} alt={image.title}></img>
             <p>{image.title}</p>
         </div>
