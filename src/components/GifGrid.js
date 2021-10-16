63351fb270b004f8ef4e3233209d13f583905a7a
Tony ID <tony.inuma@gmail.com>
2021-10-15 21:09:07 -0500

    Add: Gif Card and Grid Design

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
     const [images, setImages] = useState([]);
@@ -25,9 +26,11 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{category}</h3>
-            {images.map((img) => {
-                return <img key={img.url} src={img.url}></img>;
-            })}
+            <div className="card-grid">
+                {images.map((image) => {
+                    return <GifGridItem key={image.id} {...image} />;
+                })}
+            </div>
         </>
     );
 };
---
+++ src/components/GifGridItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const GifGridItem = (image) => {
+    return <div className="card">
+        <img src={image.url} alt={image.title}></img>
+        <p>{image.title}</p>
+    </div>;
+};
